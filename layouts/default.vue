12a6ef926edbeb0692e040d91a56868fe1d9bf7a
Thanapol Thamikanont <supermonkeyts@gmail.com>
2021-07-16 15:59:04 +0700

    first project

---
+++ layouts/default.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app dark>
+    <v-card>
+      <v-toolbar flat>
+        <v-tabs v-model="tabs" fixed-tabs>
+          <v-tabs-slider></v-tabs-slider>
+          <v-tab href="#mobile-tabs-5-1" class="primary--text">
+            <v-icon>mdi-phone</v-icon>
+          </v-tab>
+
+          <v-tab href="#mobile-tabs-5-2" class="primary--text">
+            <v-icon>mdi-heart</v-icon>
+          </v-tab>
+
+          <v-tab href="#mobile-tabs-5-3" class="primary--text">
+            <v-icon>mdi-account-box</v-icon>
+          </v-tab>
+        </v-tabs>
+      </v-toolbar>
+    </v-card>
+
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer :absolute="!fixed" app>
+      <div align="right">
+        <span>&copy; {{ new Date().getFullYear() }}</span>
+      </div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/",
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire",
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js",
+    };
+  },
+};
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-app dark>
+    <v-main class="centered">
+      <v-card>
+        <v-card-title> </v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="Username"
+            :error-messages="nameErrors"
+            label="Name"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="Password"
+            :error-messages="passwordErrors"
+            label="Password"
+            required
+          ></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <!-- <v-btn class="mr-4" color="success" nuxt to="/"> submit </v-btn> -->
+          <v-btn
+            :loading="loading5"
+            :disabled="loading5"
+            color="success"
+            class="ma-2 white--text"
+            
+            @click="loader = 'loading5'"
+          >
+            submit
+          </v-btn>
+          <v-btn @click="clear"> clear </v-btn>
+          <v-btn @click="clear"> clear </v-btn>
+
+          
+        </v-card-actions>
+      </v-card>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  layout: "full",
+  data() {
+    return {
+        loader: null,
+      loading5: false,
+    };
+  },
+  watch: {
+    loader() {
+      const l = this.loader;
+      this[l] = !this[l];
+
+      setTimeout(() => 
+        //   this[l] = false
+            this.$router.push("/")
+          , 1000);
+
+      this.loader = null;
+    },
+  },
+};
+</script>
+<style lang="css">
+.centered {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
+}
+.custom-loader {
+    animation: loader 1s infinite;
+    display: flex;
+  }
+  @-moz-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-o-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>
